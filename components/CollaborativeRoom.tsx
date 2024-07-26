f16aa18a5828da5777fb6b806e22a5e9a112493e
Douglas Moreira <dougmoreira@Douglass-MBP.lan>
2024-07-26 16:03:59 -0400

    ShareModal design

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -9,6 +9,7 @@ import { Input } from "./ui/input";
 import Image from "next/image";
 import Loader from "./Loader";
 import { updateDocument } from "@/lib/actions/room.actions";
+import ShareModal from "./ShareModal";
 
 const CollaborativeRoom = ({roomId, roomMetadata, users, currentUserType}: CollaborativeRoomProps) => {
 
@@ -95,6 +96,12 @@ const CollaborativeRoom = ({roomId, roomMetadata, users, currentUserType}: Colla
             </div>
             <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
               <ActiveCollaborators/>
+              <ShareModal 
+                roomId={roomId} 
+                collaborators={users} 
+                creatorId={roomMetadata.creatorId}
+                currentUserType={currentUserType}
+              />
               <SignedOut>
               <SignInButton />
             </SignedOut>
