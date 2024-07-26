099b463e6eec433a4b0ccdb6f8d15ba87ad7721e
Douglas Moreira <dougmoreira@Douglass-MBP.lan>
2024-07-25 23:10:47 -0400

    user mentions/

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -7,9 +7,12 @@ import {
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import Loader from "@/components/Loader";
-import { getClerkUsers } from "@/lib/actions/user.actions";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.actions";
+import { useUser } from "@clerk/nextjs";
 
 const Provider = ( {children} : { children : React.ReactNode })  => {
+  const {user: clerkUser } = useUser()
+
   return (
     <LiveblocksProvider 
         authEndpoint={"/api/liveblocks-auth"}
@@ -17,6 +20,15 @@ const Provider = ( {children} : { children : React.ReactNode })  => {
           const users = await getClerkUsers({userIds})
 
           return users
+        }}
+        resolveMentionSuggestions={ async ({text, roomId}) =>{
+            const roomUsers = await getDocumentUsers({
+              roomId,
+              currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+              text
+            })
+
+            return roomUsers
         }}
     >
       <ClientSideSuspense fallback={<Loader/>}>
